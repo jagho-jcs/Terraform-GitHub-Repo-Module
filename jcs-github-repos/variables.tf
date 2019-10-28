5e9b6ec46d58ec1a57dcb012d510f1691926d5bc
Yomi Ogunyinka <kbab6aoo@hotmail.com>
2019-10-28 15:57:16 +0000

    default values added

--- jcs-github-repos/variables.tf
+++ jcs-github-repos/variables.tf
@@ -1,8 +1,4 @@
 variable "github_organization" {}
 variable "name_of_repository" {}
 variable "description" {}
-variable "topics" {
-  type        = list(string)
-
-  description = "A list of topics to add to the repository."
-}
+variable "topics" {}
