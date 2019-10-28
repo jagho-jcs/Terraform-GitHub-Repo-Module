06d1451cf63d76ebfb7b2b78dc5705e064855e5b
Yomi Ogunyinka <kbab6aoo@hotmail.com>
2019-10-28 15:53:07 +0000

    string type changed

--- jcs-github-repos/variables.tf
+++ jcs-github-repos/variables.tf
@@ -3,6 +3,6 @@ variable "name_of_repository" {}
 variable "description" {}
 variable "topics" {
   type        = list(string)
-  default     = []
+
   description = "A list of topics to add to the repository."
 }
